41bb2448c7ad993c6dd8dc9395d76e9abcd2cc76
yangxiling <50948731+yangxiling@users.noreply.github.com>
2021-01-17 23:53:47 +0800

    update

--- 算法面试题/src/deepclone.js
+++ 算法面试题/src/deepclone.js
@@ -3,10 +3,9 @@ function deepClone(obj) {
   for (let key in obj) {
     //判断当前属性是不是对象实例上的属性   而不是原型上的属性
     if (obj.hasOwnProperty(key)) {
-      //如果当前属性是对象   就递归遍历
-      if (obj[key] === obj) {
+      if (obj[key] === obj) {//处理循环引用情况
         copy[key] = "__CIRCULAR__";
-      } else {
+      } else {      //如果当前属性是对象  就递归遍历
         copy[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key];
       }
     }
